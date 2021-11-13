bc42bcfe63a0b9873d1901df1acb34759eb0d656
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-11-13 21:43:09 +0800

    Program 2 (lowest_among_three) with time sleep.

--- prog2.py
+++ prog2.py
@@ -7,11 +7,15 @@ def inputnumber():
     secondNum = int(input("Enter second number: "))
     thirdNum = int(input("Enter third number: "))
     if firstNum < secondNum and firstNum < thirdNum:
+        time.sleep(2)
         print(f"The lowest number among the three is {firstNum}. ")
     elif secondNum < firstNum and secondNum < thirdNum:
+        time.sleep(2)
         print(f"The lowest number among the three is {secondNum}.")
     else:
+        time.sleep(2)
         print(f"The lowest number among the three is {thirdNum}.")
 
 inputnumber()
+time.sleep(1)
 print("-End-")
