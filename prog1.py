cd1513c658471dc73b6e2c21cf91b5d8178fff4b
Hannafaith B. Punzal <hannapunzal18@gmail.com>
2021-11-27 21:15:32 +0800

    Updated Program 1 (grade checker) with decimal round off.

--- prog1.py
+++ prog1.py
@@ -8,23 +8,23 @@
 def gradepercent():
     global grdpercent
     grdpercent = float(input("Enter grade percentage: "))
-    if grdpercent <= 100 and grdpercent >= 97:
+    if grdpercent <= 100 and grdpercent >= 96.5: # 97 -100
         print("Mark: 1.00 - Excellent!")
-    elif grdpercent < 97 and grdpercent >= 94:
+    elif grdpercent < 96.5 and grdpercent >= 93.5: # 94 - 96
         print("Mark: 1.25 - Excellent!")
-    elif grdpercent < 94 and grdpercent >= 91:
+    elif grdpercent < 93.5 and grdpercent >= 90.5: # 91 - 93
         print("Mark: 1.50 - Very Good!")
-    elif grdpercent < 91 and grdpercent >= 88:
+    elif grdpercent < 90.5 and grdpercent >= 87.5: # 88 - 90
         print("Mark: 1.75 - Very Good!")
-    elif grdpercent < 88 and grdpercent >= 85:
+    elif grdpercent < 87.5 and grdpercent >= 84.5: # 85 - 87
         print("Mark: 2.0 - Good")
-    elif grdpercent < 85 and grdpercent >= 82:
+    elif grdpercent < 84.5 and grdpercent >= 81.5: # 82 - 84
         print("Mark: 2.25 - Good")
-    elif grdpercent < 82 and grdpercent >= 79:
+    elif grdpercent < 81.5 and grdpercent >= 78.5: # 79 - 81
         print("Mark: 2.50 - Satisfactory")
-    elif grdpercent < 79 and grdpercent >= 76:
+    elif grdpercent < 78.5 and grdpercent >= 75.5: # 76 - 78
         print("Mark: 2.75 - Satisfactory")
-    elif grdpercent == 75:
+    elif grdpercent <= 75.4 and grdpercent >= 75: # 75 - 75.4
         print("Mark: 3.00 - Passing")
     elif grdpercent < 75 and grdpercent >= 65:
         print ("Mark: 5.00 - Failed")
